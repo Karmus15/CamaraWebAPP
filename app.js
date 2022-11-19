b938fdaaec04d856ca1758a6b72ad42535fb2c35
Karmus15 <Gamermatt15@hotmail.com>
2022-11-19 01:00:17 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var   camaraID = document.getElementById("camara"),
       botonLiga = document.getElementById("boton-liga");
       resultGlob = "";
       probGlob = null;
+      teamid = null;
       ulPlayers = document.getElementById("ul-players");
       ulTeams = document.getElementById("ul-teams");
       divbox = document.getElementById("caja-equipo");
@@ -94,8 +95,7 @@ async function fetchEquipo(formData)
 
 function tomarFoto()
 {
-
-    
+   
     sensorCamara.width = vistaCamara.videoWidth;
     sensorCamara.height = vistaCamara.videoHeight;
     sensorCamara.getContext("2d").drawImage(vistaCamara, 0, 0);
@@ -119,7 +119,7 @@ function tomarFoto()
 
 function getTeamID(nameTeam)
 {
-    dictTeamID =    [{"id": 33, "name": "manchester-united"},
+    dictTeamID =    [{"id":33, "name": "manchester-united"},
                     {"id": 34, "name": "newcastle"},
                     {"id": 38, "name": "watford"},
                     {"id": 39, "name": "wolves"},
@@ -182,9 +182,116 @@ async function showChance()
     
 }
 
+function llenarEquiposTabla(dataResponsePos)
+{
+    while (ulTeams.lastElementChild) 
+    {
+        ulTeams.removeChild(ulTeams.lastElementChild);
+    }
+
+    for (i = 0; i < 20; i++) //son siempre 20 equipos
+    {
+       let rowTeams = document.createElement("tr");
+       if (teamid == dataResponsePos[0].league.standings[0][i].team.id)
+       {
+           rowTeams.classList.add('bg-primary')
+       }
+       let rankTeam = document.createElement("th");
+       rankTeam.scope = "row";
+       rankTeam.innerHTML = i+1;
+       let imageTableTeam  = document.createElement("img");
+       imageTableTeam.src = dataResponsePos[0].league.standings[0][i].team.logo;
+       imageTableTeam.setAttribute('width', 42);
+       imageTableTeam.setAttribute('height', 42);
+       let nameTeam= document.createElement("td");
+       nameTeam.appendChild(imageTableTeam);
+       nameTeam.innerHTML += " " + dataResponsePos[0].league.standings[0][i].team.name;
+       let pjTeam = document.createElement("td");
+       pjTeam.innerHTML = dataResponsePos[0].league.standings[0][i].all.played;
+       let pgTeam = document.createElement("td");
+       pgTeam.innerHTML = dataResponsePos[0].league.standings[0][i].all.win;
+       let ptsTeam = document.createElement("td");
+       ptsTeam.innerHTML = dataResponsePos[0].league.standings[0][i].points;
+
+     //  listPlayers.classList.add("list-group-item");
+      // listPlayers.innerHTML = dataresponse[0].players[i].name;
+      rowTeams.appendChild(rankTeam);
+      rowTeams.appendChild(nameTeam);
+      rowTeams.appendChild(pjTeam);
+      rowTeams.appendChild(pgTeam);
+      rowTeams.appendChild(ptsTeam);
+       ulTeams.appendChild(rowTeams);
+    }
+
+
+
+
+}
+
+
+
+
+function traducirPosicion(pos)
+{
+    if (pos == "Goalkeeper")
+    {
+        pos = "Arquero"
+    }
+    if (pos == "Defender")
+    {
+        pos = "Defensor"
+    }
+    if (pos == "Midfielder")
+    {
+        pos = "Mediocampista"
+    }
+    if (pos == "Attacker")
+    {
+        pos = "Delantero"
+    }
+    return pos;
+}
+
+function llenarJugadoresLista(dataresponse)
+{
+
+    while (ulPlayers.lastElementChild) 
+    {
+        ulPlayers.removeChild(ulPlayers.lastElementChild);
+    }
+
+    for (i = 0; i < dataresponse[0].players.length; i++)
+    {
+       let rowPlayers = document.createElement("tr");
+       let numberPlayer = document.createElement("th");
+       numberPlayer.scope = "row";
+       numberPlayer.innerHTML = dataresponse[0].players[i].number;
+       let imagePlayer = document.createElement("img");
+       imagePlayer.src = dataresponse[0].players[i].photo;
+       imagePlayer.setAttribute('width', 20);
+       imagePlayer.setAttribute('height', 20);
+       let namePlayer = document.createElement("td");
+       namePlayer.appendChild(imagePlayer);
+       namePlayer.innerHTML += " " + dataresponse[0].players[i].name;
+       let agePlayer = document.createElement("td");
+       agePlayer.innerHTML = dataresponse[0].players[i].age;
+       let positionPlayer = document.createElement("td");
+       positionPlayer.innerHTML = traducirPosicion(dataresponse[0].players[i].position);
+
+     //  listPlayers.classList.add("list-group-item");
+      // listPlayers.innerHTML = dataresponse[0].players[i].name;
+      rowPlayers.appendChild(numberPlayer);
+      rowPlayers.appendChild(namePlayer);
+      rowPlayers.appendChild(agePlayer);
+      rowPlayers.appendChild(positionPlayer);
+       ulPlayers.appendChild(rowPlayers);
+    }
+}
+
 
 async function premierWindow()
 {
+    botonContinuar.disabled = true;
     divchance.style.visibility= "hidden"
     teamid = getTeamID(resultGlob);
 
@@ -208,23 +315,9 @@ async function premierWindow()
             'x-apisports-key': '6c45e14fbd32baba0e20e85a7427dc4e'
           }
     }) 
-
-
-
-    
     while (teamBox.lastElementChild) 
     {
         teamBox.removeChild(teamBox.lastElementChild);
-    }
-
-    while (ulPlayers.lastElementChild) 
-    {
-        ulPlayers.removeChild(ulPlayers.lastElementChild);
-    }
-
-    while (ulTeams.lastElementChild) 
-    {
-        ulTeams.removeChild(ulTeams.lastElementChild);
     }
 
     let data = await response.json();
@@ -232,100 +325,28 @@ async function premierWindow()
 
      let dataPos = await responsePosiciones.json()
      let dataResponsePos = dataPos.response;    
-   // console.log(dataresponse);
-    console.log(dataresponse[0]); 
- /*  console.log(dataResponsePos[0]);
+   /* 
+ console.log(dataresponse[0]); 
     console.log(dataResponsePos[0].league.standings[0][0].team.name);
-    console.log(dataResponsePos[0].league.standings[0][0].team.logo);
-    console.log(dataResponsePos[0].league.standings[0][0].all.played);
-    console.log(dataResponsePos[0].league.standings[0][0].all.win);
-    console.log(dataResponsePos[0].league.standings[0][0].points);*/
+    console.log(dataResponsePos[0].league.standings[0][0].points);
+    var dict = [{id: 56, name: "David De Gea", age: "36", number: "22", position: "goalkeeper", "photo": "https://media.api-sports.io/football/players/159.png" }, {id: 56, name: "David De Gea", age: "36", number: "22", position: "goalkeeper", "photo": "https://media.api-sports.io/football/players/159.png" }, {id: 56, name: "David De Gea", age: "36", number: "22", position: "goalkeeper", "photo": "https://media.api-sports.io/football/players/159.png" }, {id: 56, name: "David De Gea", age: "36", number: "22", position: "goalkeeper", "photo": "https://media.api-sports.io/football/players/159.png" }  ]
+      dict = JSON.stringify(dict);
+     console.log(dict);
+*/
+        //Llenar tabla de jugadores:
+    llenarJugadoresLista(dataresponse);
+      //llenar tabla de equipos en la liga
+    llenarEquiposTabla(dataResponsePos);
 
-    
-
-   // var dict = [{id: 56, name: "David De Gea", age: "36", number: "22", position: "goalkeeper", "photo": "https://media.api-sports.io/football/players/159.png" }, {id: 56, name: "David De Gea", age: "36", number: "22", position: "goalkeeper", "photo": "https://media.api-sports.io/football/players/159.png" }, {id: 56, name: "David De Gea", age: "36", number: "22", position: "goalkeeper", "photo": "https://media.api-sports.io/football/players/159.png" }, {id: 56, name: "David De Gea", age: "36", number: "22", position: "goalkeeper", "photo": "https://media.api-sports.io/football/players/159.png" }  ]
-     // dict = JSON.stringify(dict);
-     //console.log(dict);
-
-
-     //llenar tabla de equipos en la liga
-
-     for (i = 0; i < 20; i++) //son siempre 20 equipos
-     {
-        let rowTeams = document.createElement("tr");
-        if (teamid == dataResponsePos[0].league.standings[0][i].team.id)
-        {
-            rowTeams.classList.add('bg-primary')
-        }
-        let rankTeam = document.createElement("th");
-        rankTeam.scope = "row";
-        rankTeam.innerHTML = i+1;
-        let imageTableTeam  = document.createElement("img");
-        imageTableTeam.src = dataResponsePos[0].league.standings[0][i].team.logo;
-        imageTableTeam.setAttribute('width', 42);
-        imageTableTeam.setAttribute('height', 42);
-        let nameTeam= document.createElement("td");
-        nameTeam.appendChild(imageTableTeam);
-        nameTeam.innerHTML += " " + dataResponsePos[0].league.standings[0][i].team.name;
-        let pjTeam = document.createElement("td");
-        pjTeam.innerHTML = dataResponsePos[0].league.standings[0][i].all.played;
-        let pgTeam = document.createElement("td");
-        pgTeam.innerHTML = dataResponsePos[0].league.standings[0][i].all.win;
-        let ptsTeam = document.createElement("td");
-        ptsTeam.innerHTML = dataResponsePos[0].league.standings[0][i].points;
-
-      //  listPlayers.classList.add("list-group-item");
-       // listPlayers.innerHTML = dataresponse[0].players[i].name;
-       rowTeams.appendChild(rankTeam);
-       rowTeams.appendChild(nameTeam);
-       rowTeams.appendChild(pjTeam);
-       rowTeams.appendChild(pgTeam);
-       rowTeams.appendChild(ptsTeam);
-        ulTeams.appendChild(rowTeams);
-     }
-
-
-     //Llenar tabla de jugadores:
-     for (i = 0; i < dataresponse[0].players.length; i++)
-     {
-        let rowPlayers = document.createElement("tr");
-        let numberPlayer = document.createElement("th");
-        numberPlayer.scope = "row";
-        numberPlayer.innerHTML = dataresponse[0].players[i].number;
-        let imagePlayer = document.createElement("img");
-        imagePlayer.src = dataresponse[0].players[i].photo;
-        imagePlayer.setAttribute('width', 20);
-        imagePlayer.setAttribute('height', 20);
-        let namePlayer = document.createElement("td");
-        namePlayer.appendChild(imagePlayer);
-        namePlayer.innerHTML += " " + dataresponse[0].players[i].name;
-        let agePlayer = document.createElement("td");
-        agePlayer.innerHTML = dataresponse[0].players[i].age;
-        let positionPlayer = document.createElement("td");
-        positionPlayer.innerHTML = dataresponse[0].players[i].position;
-
-      //  listPlayers.classList.add("list-group-item");
-       // listPlayers.innerHTML = dataresponse[0].players[i].name;
-       rowPlayers.appendChild(numberPlayer);
-       rowPlayers.appendChild(namePlayer);
-       rowPlayers.appendChild(agePlayer);
-       rowPlayers.appendChild(positionPlayer);
-        ulPlayers.appendChild(rowPlayers);
-     }
-
-//var teamTag = document.createElement("p");
 let imageTeam = document.createElement("img");
 imageTeam.src = dataresponse[0].team.logo;  //response[0].team.logo
 imageTeam.style = "position:relative ;top:5px; max-width: 100%; max-height: 100%; width: 75px; height: 75px"
 
 var teamName = document.createTextNode(dataresponse[0].team.name)
 
-
-
 var teamTag = document.createElement("p");
 teamTag.id = "titulo-equipo"
 teamTag.style = "position:relative;top:5px; text-align: center; width: 100%"
-
 
 divbox.style.visibility = "visible";
 
